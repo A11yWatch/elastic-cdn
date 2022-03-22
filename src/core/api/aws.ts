b175f2d5eff24b8932445cceed0f54f62f1142f4
j-mendez <jeff@a11ywatch.com>
2022-03-22 11:57:08 -0400

    chore(minifier): add rust async minifier

--- src/core/api/aws.ts
+++ src/core/api/aws.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) A11yWatch, LLC. and its affiliates.
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- **/
-
 import { S3 } from "aws-sdk";
 import fs from "fs";
 import { Response } from "express";
