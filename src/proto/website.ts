4c622468f654f024eda60a8abb814d47882a54e0
j-mendez <jeff@a11ywatch.com>
2023-01-06 19:02:19 -0500

    chore(rpc): fix schema import local

--- src/proto/website.ts
+++ src/proto/website.ts
@@ -24,7 +24,7 @@ export interface Service {
 export const getProto = async (
   target = "/cdn.proto"
 ): Promise<Service & GRPC> => {
-  const packageDef = await load(`node_modules/@a11ywatch/protos${target}`, {
+  const packageDef = await load(`./node_modules/@a11ywatch/protos${target}`, {
     keepCase: true,
     longs: String,
     enums: String,
